a1f76bc3065166ded2ab10a4dcb904a650689862
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-06-05 09:49:48 +0530

    Time: 0 ms (100%), Space: 12.2 MB (11.2%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int n = arr.size();
-        vector<bool> visited(n, false);
-        vector<int> counts;
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == true)
-                continue;
-            int count = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] == arr[j]) {
-                    visited[j] = true;
-                    count++;
-                }
-            }
-            counts.push_back(count);
+        unordered_map<int,int> freq;
+        for(int num : arr){
+            freq[num]++;
         }
-        bool flag = true;
-        for(int i=0 ; i<counts.size() ; i++){
-            for(int j=i+1 ; j<counts.size() ; j++){
-                if(counts[i] == counts[j]){
-                    flag = false;
-                }
-            }
+        unordered_set<int> seen;
+        for(auto& pair : freq){
+            if(seen.count(pair.second))
+                return false;
+            seen.insert(pair.second);
         }
-        if(!flag){
-            return false; 
-        }
-        else{
-            return true;
-        }
+        return true;
+        
     }
 };
