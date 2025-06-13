d597146c68d065b9794e7165d1562d268935d7da
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-06-13 09:22:00 +0530

    Time: 27 ms (69.99%), Space: 57.3 MB (23.71%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
+        int res = 0;
         int n = piles.size();
-        int count = 0;
-        int maxsum = 0;
-        int limit = (n/3);
         sort(piles.begin(),piles.end());
-        for(int i=(n-2) ; i>=0 ; i = i-2){
-            maxsum += piles[i];
-            count++;
-            if(count == limit)  break;
+        for(int i=(n/3) ; i<n ; i=i+2){
+            res += piles[i];
         }
-        return maxsum;
+        return res;
+
     }
 };
