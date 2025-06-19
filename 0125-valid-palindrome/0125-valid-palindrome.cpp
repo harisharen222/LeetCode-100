73b79e27cd905748f4b798a80d48ce96ae23ceac
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-06-19 17:38:35 +0530

    Time: 3 ms (31.39%), Space: 9.8 MB (94.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    bool isAlphaNum(char ch){
-        if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z' )){
-            return true;
-        }
-        return false;
-    }
     bool isPalindrome(string s) {
         int st = 0 , end = s.length() - 1;
         while(st < end){
-            if(!isAlphaNum(s[st])){
+            if(!isalnum(s[st])){
                 st++;
                 continue;
             }
-            if(!isAlphaNum(s[end])){
+            if(!isalnum(s[end])){
                 end--;
                 continue;
             }
