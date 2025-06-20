97dcaf18e7be582f3cfc72ab759963a58fc4229b
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-06-20 11:34:58 +0530

    Time: 199 ms (24.94%), Space: 23.5 MB (12.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isFreqSame(vector<int>& freq1,vector<int>& freq2){
+    bool isfreqSame(vector<int>& freq1 , vector<int>& freq2){
         for(int i=0 ; i<26 ; i++){
             if(freq1[i] != freq2[i]){
                 return false;
@@ -9,21 +9,23 @@ public:
         return true;
     }
     bool checkInclusion(string s1, string s2) {
-        vector<int>freq1(26,0);
-        
-        for(int i=0 ; i<s1.size() ; i++){
-            freq1[s1[i] - 'a']++;
+        vector<int> freq(26,0);
+        for(int i=0 ; i<s1.length() ; i++){
+            freq[s1[i]-'a']++;
         }
         int window_size = s1.length();
-        for(int i=0 ; i<s2.length() ; i++){
-            int window_idx = 0, idx = i;
-            vector<int>freq2(26,0);
 
+        for(int i=0 ; i<s2.length() ; i++){
+            vector<int> freq2(26,0);
+            int window_idx = 0, idx = i;
             while(window_idx < window_size && idx < s2.length()){
-                freq2[s2[idx] - 'a']++;
-                idx++;window_idx++;
+                freq2[s2[idx]-'a']++;
+                window_idx++;
+                idx++;
             }
-            if(isFreqSame(freq1,freq2))  return true;
+            if(isfreqSame(freq,freq2)){
+                return true;
+            }
         }
         return false;
     }
