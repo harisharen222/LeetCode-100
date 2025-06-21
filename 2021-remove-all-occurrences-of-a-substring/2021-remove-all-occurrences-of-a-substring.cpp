50f90cc9d14533aee3db747ce14625bac8a46e43
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-06-21 15:20:53 +0530

    Time: 0 ms (100%), Space: 9.5 MB (39.45%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length()>0 && s.find(part) < s.length()){
-            s.erase(s.find(part),part.length());
+        size_t len = s.find(part);
+        while(len < s.length()){
+            s.erase(len,part.length());
+            len = s.find(part);
         }
         return s;
     }
