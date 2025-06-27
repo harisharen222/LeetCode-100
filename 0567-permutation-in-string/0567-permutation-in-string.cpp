713bfb7e016c9cc85daf3165577b1438e2675792
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-06-27 11:56:51 +0530

    Time: 2 ms (72.47%), Space: 10 MB (68.04%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    bool checkequal(int count1[26],int count2[26]){
-        for(int i=0 ; i<26 ; i++){
-            if(count1[i] != count2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
+        vector<int> freq(26,0);
+        vector<int> freq2(26,0);
         if(s1.length() > s2.length())   return false;
-        int count1[26] = {0};
-        int count2[26] = {0};
         for(int i=0 ; i<s1.length() ; i++){
-            int index = s1[i]-'a';
-            count1[index]++;
-            index = s2[i]-'a';
-            count2[index]++;
-            
+            freq[s1[i]-'a']++;
+            freq2[s2[i]-'a']++;
         }
-        if(checkequal(count1,count2))   return true;
-        int window_size = s1.length();
-        for(int i=s1.length() ; i<s2.length(); i++){
+        if(freq == freq2)   return true;
+        int window = s1.length();
+        for(int i=s1.length() ; i<s2.length() ; i++){
+            int index = s2[i]-'a';
+            freq2[index]++;
             
-            int index = s2[i] - 'a';
-            count2[index]++;
-            int oldindex = s2[i-window_size]-'a';
-            count2[oldindex]--;
-            if(checkequal(count1,count2))   return true;
+            int oldidx = s2[i-window]-'a';
+            freq2[oldidx]--;
+
+            if(freq == freq2)   return true;
         }
         return false;
     }
