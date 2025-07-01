e1c69f1e56e043e1b4ac2f7bf51d8a882d1b14d4
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-07-02 01:23:57 +0530

    Time: 0 ms (100%), Space: 10.4 MB (61.64%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        deque<string> q;
         int st = 0;
         int end = 0;
-        deque<string> dq;
-        while(st < s.length() && s[st] == ' '){
+        while(st < s.length() && s[st] == ' ' ){
             st++;
-            end = st;
-        }
-        while(end < s.length()){
-            if(s[end] == ' ' || s[end] == '\0'){
-                string word = s.substr(st,end-st);
-                dq.push_front(word);
-                while(end < s.length() && s[end] == ' '){
-                    end++;
-                    st = end;
-                }
-            }
             end++;
         }
-        if(st < s.length()){
-            string word = s.substr(st,end-st);
-            dq.push_front(word);
+        while(end < s.length()){
+            if(s[end] == ' '){
+                if(st<end){
+                    string str = s.substr(st,end-st);
+                    q.push_front(str);
+                }
+                while(s[end] == ' ' && end < s.length()){
+                    end++;
+                }
+                st = end;
+            }
+            else    end++;    
+        }
+        if (st < s.length()) {
+            string str = s.substr(st, end - st);
+            q.push_front(str);
         }
         string result = "";
-        while(!dq.empty()){
-            result += dq.front();
-            dq.pop_front();
-            if(!dq.empty()) result += " ";
+        while(!q.empty()){
+            result += q.front();
+            q.pop_front();
+            if(!q.empty()){
+                result += ' ';
+            }
         }
         return result;
     }
