55c18d391367ba6e0a9bfa9ec25a8bdfffccf21d
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-07-02 11:13:03 +0530

    Time: 1 ms (84.72%), Space: 14 MB (55.27%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1 || s.size() <= numRows)
-            return s;
+        if(s.length() <= numRows || numRows == 1)    return s;
         vector<string>rows(numRows);
-        bool goingdown = false; 
         int curr_row = 0;
+        bool goingdown = false;
         for(char c : s){
             rows[curr_row] += c;
             if(curr_row == 0 || curr_row == numRows-1){
                 goingdown = !goingdown;
             }
-            if(goingdown){
-                curr_row++;
-            }
-            else    curr_row--;
+            goingdown ? curr_row++ : curr_row--;
         }
         string result = "";
-        for(string row : rows){
-            result += row;
+        for(string s : rows){
+            result += s;
         }
         return result;
+
     }
 };
