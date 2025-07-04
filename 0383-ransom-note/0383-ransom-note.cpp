cd147c8b80921578921febe2c9b13aa4361c20ae
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-07-05 02:32:26 +0530

    Time: 4 ms (49.21%), Space: 12 MB (29.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>freq;
-        for(char s : magazine){
-            freq[s]++;
+        unordered_map<char, int> freq;
+        if (magazine.length() < ransomNote.length())
+            return false;
+        for (char c : magazine) {
+            freq[c]++;
         }
-        for(char c : ransomNote){
-            if(freq[c] == 0){
+        for (char c : ransomNote) {
+            if (freq[c] == 0)
                 return false;
-            }
             freq[c]--;
         }
         return true;
