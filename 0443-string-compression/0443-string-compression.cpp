b07170be504b4949b24b94c1b437c3ffcfd47a22
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-07-10 19:35:55 +0530

    Time: 0 ms (100%), Space: 13.7 MB (74.18%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -8,18 +8,19 @@ public:
         while(reader < n){
             char current = chars[reader];
             int count = 0;
-            while(reader < n && chars[reader] == current){
+            while( reader < n && chars[reader] == current){
                 reader++;
                 count++;
             }
+
             chars[writer++] = current;
-            string countstr = to_string(count);
             if(count > 1){
-                for(char ch : countstr){
-                    chars[writer++] = ch;
+                string countstr = to_string(count);
+                for(char c : countstr){
+                    chars[writer++] = c;
                 }
             }
-
+            
         }
         return writer;
     }
