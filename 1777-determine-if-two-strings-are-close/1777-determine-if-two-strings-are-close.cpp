f1e1413a7681a0558319e21023829861ff85c902
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-07-12 02:56:34 +0530

    Time: 7 ms (80.37%), Space: 23.3 MB (87.13%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -4,19 +4,20 @@ public:
         if (word1.size() != word2.size()) return false;
         vector<int> freq1(26);
         vector<int> freq2(26);
-        vector<bool> set1(26);
-        vector<bool> set2(26);
+ 
         for(char c : word1){
             freq1[c-'a']++;
-            set1[c-'a'] = true;
+
         }
         for(char c : word2){
             freq2[c-'a']++;
-            set2[c-'a'] = true;
+
         }
 
         
-        if(set1 != set2)    return false;
+        for (int i = 0; i < 26; ++i) {
+            if ((freq1[i] == 0) != (freq2[i] == 0)) return false;
+        }
         
         sort(freq1.begin(),freq1.end());
         sort(freq2.begin(),freq2.end());
