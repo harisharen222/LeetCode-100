e5f914610a3d35cddf3b2805b4b370c1b81e5dcd
harisharen222 <72202792+harisharen222@users.noreply.github.com>
2025-07-17 22:42:17 +0530

    Time: 295 ms (55.35%), Space: 67 MB (93.67%) - LeetHub

---
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    condition = (products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')
+    return products[condition][['product_id']]
