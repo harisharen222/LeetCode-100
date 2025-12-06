8e54a18e624faf140d7c7fc85e1847160582e3ac
Hari Sharen <72202792+harisharen222@users.noreply.github.com>
2025-12-07 02:13:19 +0530

    Time: 9 ms (19.67%), Space: 24 MB (34.81%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        if(intervals.empty()) return {};
 
-        sort(intervals.begin(),intervals.end());
+        if (intervals.empty()) 
+            return {};
+
+        sort(intervals.begin(), intervals.end());
+
         vector<vector<int>> merged;
-
         merged.push_back(intervals[0]);
 
-        for(int i=1 ; i<intervals.size() ; i++){
-            vector<int>& last = merged.back();     
-            const vector<int>& current = intervals[i];
+        for (int i = 1; i < intervals.size(); i++) {
 
-
-            if(current[0] <= last[1]){
-                last[1] = max(last[1],current[1]);
+            if (intervals[i][0] <= merged.back()[1]) {
+                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
             } else {
-                merged.push_back(current);
+                merged.push_back(intervals[i]);
             }
         }
+
         return merged;
     }
 };
