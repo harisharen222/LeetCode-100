38377bc34bb3b3013dd1fa1b6bafe2486802e3f6
Hari Sharen <harisharen88@GMAIL.COM>
2026-04-25 01:32:04 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int st = 0 ;
-        int end = nums.size() - 1;
-        while(st<=end){
-            int mid = st + (end-st)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[st]<=nums[mid]){
-                if(nums[st]<= target && target<= nums[mid])
-                    end = mid-1;
-                else    
-                    st = mid+1;
-            }
-            else{
-                if(nums[mid]<=target && target<= nums[end]){
-                    st = mid+1;
-                }
-                else{
-                    end = mid -1;
-                }
+    int getPivot(vector<int>& nums){
+        int low = 0, high = nums.size() - 1;
+
+        while(low < high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] > nums[high]){
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
+        return low;
+    }
+
+    int binary_search(vector<int>& nums, int low, int high, int target){
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
         return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int pivot = getPivot(nums);
+        int n = nums.size();
+
+        // if array not rotated
+        if(pivot == 0){
+            return binary_search(nums, 0, n-1, target);
+        }
+
+        if(target >= nums[pivot] && target <= nums[n-1]){
+            return binary_search(nums, pivot, n-1, target);
+        } else {
+            return binary_search(nums, 0, pivot-1, target);
+        }
     }
 };
