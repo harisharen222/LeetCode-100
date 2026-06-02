4b3c1181eb5f20e67a48442b59057a60126fafb1
Hari Sharen <harisharen88@GMAIL.COM>
2026-06-02 22:27:06 +0530

    Time: 0 ms (100%), Space: 10 MB (70.52%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> freq(26,0);
+        vector<int> freq1(26,0);
         vector<int> freq2(26,0);
         if(s1.length() > s2.length())   return false;
         for(int i=0 ; i<s1.length() ; i++){
-            freq[s1[i]-'a']++;
-            freq2[s2[i]-'a']++;
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
         }
-        if(freq == freq2)   return true;
+        if(freq1 == freq2)  return true;
         int window = s1.length();
-        for(int i=s1.length() ; i<s2.length() ; i++){
-            int index = s2[i]-'a';
+        for(int i = s1.length() ; i < s2.length() ; i++){
+            int index = s2[i] - 'a';
             freq2[index]++;
-            
-            int oldidx = s2[i-window]-'a';
-            freq2[oldidx]--;
 
-            if(freq == freq2)   return true;
+            int oldidx = s2[i-window] - 'a';
+            freq2[oldidx]--;
+            if(freq1 == freq2)  return true;  
         }
         return false;
+
+
     }
 };
